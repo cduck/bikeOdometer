b01751a4e0b59d961c332de5eef3e15125992e44
dwai213 <dwai213@gmail.com>
2015-11-15 01:24:17 -0800

    Commit of LSM303 Acc/Mag library for mbed. Modified main code to also log measured data

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "SDFileSystem.h"
 #include "AutoSDLogger.h"
 #include "LPS.h"
+#include "LSM303.h"
 
 SDFileSystem sd(PTE3, PTE1, PTE2, PTE4, "sd"); // MOSI, MISO, SCK, CS
 AutoSDLogger autoSD;
@@ -12,27 +13,15 @@ FILE *fp;
 
 I2C i2c(PTE25, PTE24);
 LPS ps(&i2c);
+LSM303 acc(&i2c);
 
 int main() {
     pc.printf("Starting \r\n");
 
-    // Sample Code to Write Data
-    // autoSD.ready_datalogger(); // Prepares a datalog filename according to index.txt
-    // pc.printf("Index: %d",autoSD.curr_index());
-    
-    // printf("\nWriting data to the sd card \r\n");
-    // fp = fopen(autoSD.filepath, "w");
-    // if (fp == NULL) {
-    //     pc.printf("Unable to write the file \r\n");
-    // }
-    // for ( int i = 0; i < 100; i++){        
-    //     fprintf(fp, "%f, %f, %f \r\n",sensor.read(),sensor.read()+.1,sensor.read()+.05);
-    // }
-    // fclose(fp);
-    
-    // pc.printf("File successfully written! \r\n");
+    autoSD.ready_datalogger(); // Prepares a datalog filename according to index.txt
+    pc.printf("Index: %d",autoSD.curr_index());         
 
-    // Sample Code to Test Barometer
+    // Sample Code to Test 10 DOF sensor
     if(!ps.init()){
         pc.printf("Unable to talk to Barometer\r\n");
         while(1);
@@ -42,10 +31,33 @@ int main() {
         ps.enableDefault();
     }
 
-    while(1){
+    if(!acc.init()){
+        pc.printf("Unable to talk to Acc/Mag\r\n");
+        while(1);
+    }
+    else{
+        pc.printf("Success in talking to Acc/Mag! \r\n");
+        acc.enableDefault();
+    }    
+    printf("\nWriting data to the SD card \r\n");
+    fp = fopen(autoSD.filepath, "w");
+    if (fp == NULL) {
+        pc.printf("Unable to write the file \r\n");
+    }
+    for ( int i = 0; i < 100; i++){
+
+        acc.read();
         float pressure = ps.readPressureMillibars();
         float altitude = ps.pressureToAltitudeMeters(pressure);
-        pc.printf("Pressure: %f mBar Altitude: %f m\r\n",pressure,altitude);
+        pc.printf("Pressure: %f mBar Altitude: %f m A: %6d %6d %6d M: %6d %6d %6d \r\n",
+            pressure,altitude,acc.a.x,acc.a.y,acc.a.z,
+            acc.m.x,acc.m.y,acc.m.z);
+        fprintf(fp, "%f, %f, %d, %d, %d, %d, %d, %d",
+            pressure,altitude,acc.a.x,acc.a.y,acc.a.z,
+            acc.m.x,acc.m.y,acc.m.z);
+        // wait_ms(50);
     }
+    fclose(fp);
+    pc.printf("File successfully written! \r\n");
     printf("End of Program. \r\n");
 }
