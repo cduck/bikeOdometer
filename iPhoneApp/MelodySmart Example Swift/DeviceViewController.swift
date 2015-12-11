8817a6403c429c59e55a818ccf436ec9388a008b
Casey Duckering <cduckering@gmail.com>
2015-12-11 03:50:11 -0800

    iPhone display speed, incline, distance, altitude from sensor

--- iPhoneApp/MelodySmart Example Swift/DeviceViewController.swift
+++ iPhoneApp/MelodySmart Example Swift/DeviceViewController.swift
@@ -6,6 +6,36 @@
 //  Copyright (c) 2015 BlueCreation. All rights reserved.
 //
 
+import Foundation
+
+extension String {
+    subscript (r: Range<Int>) -> String {
+        get {
+            let startIndex = self.startIndex.advancedBy(r.startIndex)
+            let endIndex = self.startIndex.advancedBy(r.endIndex)
+            
+            return self[Range(start: startIndex, end: endIndex)]
+        }
+    }
+}
+
+func hexStrToInt(str: String) -> Int? {
+    if var a = Int(str, radix: 16) {
+        a & 0x8000
+        if a & 0x8000 > 0 {
+            a = -((a ^ 0xFFFF) + 1)
+        }
+        return a
+    }
+    return nil
+}
+
+
+
+
+
+
+
 import UIKit
 
 class MelodyController: UIViewController {
@@ -17,6 +47,11 @@ class DeviceViewController: UIViewController, MelodySmartDelegate, UITextFieldDe
 
     @IBOutlet var tfIncomingData: UITextField!
     @IBOutlet var tfOutgoingData: UITextField!
+    
+    @IBOutlet var altField: UITextField!
+    @IBOutlet var inclineField: UITextField!
+    @IBOutlet var distField: UITextField!
+    @IBOutlet var speedField: UITextField!
 
     private weak var i2cController: I2cCommandsViewController?
     private weak var commandsController: RemoteCommandsViewController?
@@ -47,7 +82,36 @@ class DeviceViewController: UIViewController, MelodySmartDelegate, UITextFieldDe
     }
 
     func melodySmart(melody: MelodySmart!, didReceiveData data: NSData!) {
-        tfIncomingData.text = NSString(data: data, encoding: NSUTF8StringEncoding) as? String
+        let str = NSString(data: data, encoding: NSUTF8StringEncoding)
+        tfIncomingData.text = str as? String
+        let trimmed = str!.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        let len = trimmed.characters.count
+        print("Trimmed length: \(trimmed.characters.count)")
+        if len == 17 && trimmed.hasPrefix("x") {
+            let altStr = trimmed[1..<5]
+            let incStr = trimmed[5..<9]
+            let distStr = trimmed[9..<13]
+            let speedStr = trimmed[13..<17]
+            if let altVal = hexStrToInt(altStr) {
+                let alt = Double(altVal) / 100.0
+                self.altField.text = "\(alt) meters"
+                print("Success, \(altStr), \("\(alt) meters")");
+            } else{
+                print("Fail, \(altStr)");
+            }
+            if let incVal = hexStrToInt(incStr) {
+                let inc = Double(incVal) / 100.0
+                self.inclineField.text = "\(inc) rad"
+            }
+            if let distVal = hexStrToInt(distStr) {
+                let dist = Double(distVal) / 100.0
+                self.distField.text = "\(dist) meters"
+            }
+            if let speedVal = hexStrToInt(speedStr) {
+                let speed = Double(speedVal) / 100.0
+                self.speedField.text = "\(speed) meters/sec"
+            }
+        }
     }
 
     func melodySmart(melody: MelodySmart!, didReceivePioChange state: UInt8, withLocation location: BcSmartPioLocation) {
