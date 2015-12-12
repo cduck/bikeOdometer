875982261b3be7aa10a69870fd23398ac3ceac5e
dwai213 <dwai213@gmail.com>
2015-12-12 04:19:45 -0800

    Added TokenToSTL functionality

--- Server/tokenizer.py
+++ Server/tokenizer.py
@@ -1,9 +1,24 @@
+
 # coding: utf-8
+
+# In[451]:
+
 import string
 
+
+# In[452]:
+
 queries = [
-    'Did my average speed exceed 5 m/s?'
+    'Did my average speed exceed 5 m/s?', #F(average > 5)
+    'Did I bike more than 500 meters this week?', #F(tot_dist > 500)
+    'Did I always stay above an altitude of 50 meters?', #G(attitude > 50)
+    'Did my speed ever exceed 10 m/s?', #F(attitude > 50)
+    'Was I always faster than 10 m/s?' #G(speed > 10)
+    'Did my speed exceed 5 m/s and my altitude stayed above 
 ]
+
+
+# In[453]:
 
 triggerPhrases = [
     ['did i', 'do i'],
@@ -11,11 +26,13 @@ triggerPhrases = [
 ]
 # Should contain all //recognizable// words. 
 synonyms = [
-    ['maintain'],
+    ['maintain'], #GF or FG?
+    ['stay','always'], #G
+    ['did','ever'], #F
+    ['greater','exceed','more','faster','above'], # >
+    ['less','below','slower'], # <
     ['after'],
     ['until'],
-    ['greater'],
-    ['less'],
     ['speed', 'velocity'],
     ['distance'],
     ['incline', 'hill'],
@@ -29,16 +46,20 @@ synonyms = [
     ['meter'],
     ['foot'],
     ['of'],
-    ['did']
+    ['average','mean'],
+    ['bike','travel']
 ]
 unitWords = [  # Omit s if can be plural
     'degree', 'mph', 'second', 'minute', 'hour', 'meter', 'foot', 'meters per second'
 ]
+modifierWords = [  # Words that modify units
+    'average','attitude','bike'
+]
 connectingWords = [
     'of'
 ]
-operatorWords = [
-    'maintain', 'after', 'until', 'greater', 'less', 'did'
+operatorWords = [ # Order matters in this array
+    'maintain', 'after', 'until', 'greater', 'less', 'did','stay'
 ]
 variableWords = [
     'speed', 'altitude', 'distance', 'incline'
@@ -61,7 +82,8 @@ operatorForms = {
     # ...
 }
 
-#######TOKENIZER CODE##################################################################
+
+# In[454]:
 
 # Create synonymDict mapping all synonyms to their canonical word
 synonymDict = {}
@@ -70,14 +92,19 @@ for group in synonyms:
     for word in group:
         synonymDict[word] = standardWord
 
+
+# In[455]:
+
 all = string.maketrans('','')
 noDigs = all.translate(all, string.digits)
 noLetters = all.translate(all, string.letters)
 allowedLetters = ' abcdefghijklmnopqrstuvwxyz0123456789%.º°'
 notAllowed = all.translate(all, allowedLetters)
-
 def cleanQuery(query):
     return query.lower().translate(all, notAllowed)
+
+
+# In[456]:
 
 # Attempts to return the canonical words in your query
 def wordToStandard(word):
@@ -98,7 +125,14 @@ def wordToStandard(word):
         word = word[:-1]
         if word in synonymDict:
             return synonymDict[word]
+    if word[-2:] == 'ed':
+        word = word[:-2]
+        if word in synonymDict:
+            return synonymDict[word]
     return None
+
+
+# In[457]:
 
 class Token:
     '''A word associated with its meaning'''
@@ -110,6 +144,7 @@ class Token:
     NUMBER = 4
     UNIT = 5
     VARIABLE = 6
+    MODIFIER = 7
     def __init__(self, word, left=None, right=None):
         self.origWord = word
         self.word = wordToStandard(word)
@@ -117,6 +152,8 @@ class Token:
         self.right = wordToStandard(right)
         if self.word in operatorWords:
             self.type = Token.OPERATOR
+        elif self.word in modifierWords:
+            self.type = Token.MODIFIER
         elif self.word in variableWords:
             self.type = Token.VARIABLE
         elif self.word in unitWords:
@@ -131,10 +168,52 @@ class Token:
                 print repr(self.origWord), repr(self.word), 'not understood.'
     def __str__(self):
         typeStr = ['UNKNOWN', 'NONE', 'OPERATOR', 'CONNECTOR',
-                   'NUMBER', 'UNIT', 'VARIABLE'][self.type]
+                   'NUMBER', 'UNIT', 'VARIABLE','MODIFIER'][self.type]
         return '<%s, %s>' % (self.word, typeStr)
     def __repr__(self):
         return self.__str__()
+
+
+# In[458]:
+
+class TokenGroup:
+    '''A group of tokens that mean somthing as a group'''
+    # Group types
+    UNKOWN = 100
+    VALUE = 101  # Number with unit
+    BOOL_EXPR = 102  # Logical or temporal
+    REAL_EXPR = 103
+    tokenPatterns = {
+        VALUE: [
+            [Token.NUMBER, Token.UNIT],
+            [Token.NUMBER]
+        ],
+        BOOL_EXPR: [
+            # Equality operator
+            [Token.VARIABLE, Token.CONNECTOR, VALUE],
+            [VALUE, Token.VARIABLE],
+            []
+        ]
+    }
+    def __init__(self, type, tokens):
+        self.type = type
+        self.tokens = tokens
+    def __str__(self):
+        typeStr = ['UNKNOWN', 'VAL', 'EXPRESSION'][self.type-100]
+        if self.type == TokenGroup.UNKNOWN:
+            return '<TokenGroup UNKNOWN>'
+        if self.type == TokenGroup.VALUE:
+            return '<VAL %s %s>' 
+        if self.type == TokenGroup.BOOL_EXPR:
+            return '<EXPR>'
+        if self.type == TokenGroup.REAL_EXPR:
+            return '<EXPR>'
+            #return '<%s, %s>' % (repr(self.word), typeStr)
+    def __repr__(self):
+        return self.__str__()
+
+
+# In[459]:
 
 # This gives you the recognized canonical words
 def splitQuery(query):
@@ -147,7 +226,14 @@ def splitQuery(query):
             tokens.append(standard)
     return tokens
 
-splitQuery(queries[0])
+
+# In[460]:
+
+print splitQuery(queries[0])
+Token('until')
+
+
+# In[461]:
 
 # This will tell you what type each splitted query is
 def tokenizeQuery(query):
@@ -162,5 +248,120 @@ def tokenizeQuery(query):
     return tokens
 
 
+# In[462]:
 
+# This will take an array of tokens and then output a dictionary index by Token Types
+def splitTokensByType(tokens):
+    # Token types, copied from Token definition
+    UNKNOWN = 0
+    NONE = 1
+    OPERATOR = 2
+    CONNECTOR = 3
+    NUMBER = 4
+    UNIT = 5
+    VARIABLE = 6
+    MODIFIER = 7
+
+    splitToks = {}
+    #Separate out tokens by type
+    for token in tokens:
+        if token.type == OPERATOR:
+            if 'OPERATOR' in splitToks and (token.word not in splitToks['OPERATOR']):
+                splitToks['OPERATOR'].append(token.word)
+            else:
+                splitToks['OPERATOR'] = [token.word]
+        elif token.type == UNIT:
+            splitToks['UNIT'] = token.word
+        elif token.type == VARIABLE:
+            splitToks['VARIABLE'] = token.word
+        elif token.type == MODIFIER:
+            if 'MODIFIER' in splitToks:
+                splitToks['MODIFIER'].append(token.word)
+            else:
+                splitToks['MODIFIER'] = [token.word]
+        elif token.type == NUMBER:
+            splitToks['NUMBER'] = token.word
+        else:
+            print token.type
+            print 'Error'
+    return splitToks
+
+def determineVariable(splitToks):
+    if 'VARIABLE' in splitToks: #easy way out
+        return splitToks['VARIABLE']
+    else:
+        variable = ''
+        if 'MODIFIER' in splitToks:
+            if splitToks['MODIFIER'] == 'average':
+                variable = 'average'
+        if 'UNIT' in splitToks:
+            if splitToks['UNIT'] == 'meters per second':
+                variable = variable + ' speed'
+            elif splitToks['UNIT'] == 'meter':
+                variable = variable + 'distance'
+        return variable
+
+
+# In[463]:
+
+def tokenToSTL(tokens):
+    #Assume only one variable
+    splitToks = splitTokensByType(tokens)
+    numOperators = len(splitToks['OPERATOR'])
+    for i in xrange(numOperators):        
+        print 'i: %d'%i
+        min_power = 100
+        min_operator = ''
+        variable = determineVariable(splitToks)
+        print 'var: %s'%variable
+        #Choose the most bad ass operator
+        for j in xrange(len(splitToks['OPERATOR'])):            
+            print len(splitToks['OPERATOR'])
+            operator = splitToks['OPERATOR'][j]
+            power = operatorWords.index(operator)
+            if power < min_power:
+                min_power = power
+                min_operator = operator
+        #Apply the bad ass operator
+        if variable == '':
+            if min_operator == 'did':
+                STL = ('F',STL)
+            elif min_operator == 'stay':
+                STL = ('G',STL)            
+        else:
+            if min_operator == 'greater':
+                STL = ('>',variable,splitToks['NUMBER'])
+            elif min_operator == 'less':
+                STL = ('<',variable,splitToks['NUMBER'])
+        rem_tokens = []
+        if variable != '':            
+            for token in tokens:
+                if token.word == variable:
+                    pass
+                elif token.word == splitToks['NUMBER']:
+                    pass
+                elif token.word == splitToks['UNIT']:
+                    pass
+                elif token.word == min_operator:
+                    pass
+                else:
+                    rem_tokens.append(token)
+        else:
+            if token.word == min_operator:
+                pass
+            else:
+                rem_tokens.append(token)
+        splitToks = splitTokensByType(rem_tokens)
+        print splitToks
+    return STL
+
+
+# In[468]:
+
+ind = 3
+print queries[ind]
+tokens = tokenizeQuery(queries[ind])
+splitToks = splitTokensByType(tokens)
+print splitToks
+tokenToSTL(tokens)
 
